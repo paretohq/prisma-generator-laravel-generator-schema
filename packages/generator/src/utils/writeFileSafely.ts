561e6d709f65946824273dab38e7a7722828a187
Mosaab_Emam <mosaab.emam123@gmail.com>
2023-02-18 20:32:38 +0200

    feat: cleanup & basic json file with defaults

--- packages/generator/src/utils/writeFileSafely.ts
+++ packages/generator/src/utils/writeFileSafely.ts
@@ -2,10 +2,12 @@ import fs from 'fs'
 import path from 'path'
 import { formatFile } from './formatFile'
 
-export const writeFileSafely = async (writeLocation: string, content: any) => {
+export const writeFileSafely = async (writeLocation: string, content: object) => {
   fs.mkdirSync(path.dirname(writeLocation), {
     recursive: true,
   })
 
-  fs.writeFileSync(writeLocation, await formatFile(content))
+  // fs.writeFileSync(writeLocation, await formatFile(content))
+
+  fs.writeFileSync(writeLocation, JSON.stringify(content))
 }
