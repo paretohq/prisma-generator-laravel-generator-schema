ca2930be0f59a1f4d195afd5ed3125615ae02a01
Mosaab_Emam <mosaab.emam123@gmail.com>
2023-02-19 09:12:15 +0200

    feat(getDbType): infer db type from field type

--- packages/generator/src/helpers/getDbType.ts
+++ packages/generator/src/helpers/getDbType.ts
@@ -1,5 +1,17 @@
 import { DMMF } from "@prisma/generator-helper";
 
-export function getDbType(field: DMMF.Field): string {
-  return field.name
+export function getDbType(field: DMMF.Field): String {
+  if (field.type === "String") return 'string';
+  if (field.type === "DateTime") return 'datetime';
+  if (field.type === "JSON") return 'json';
+  if (field.type === "Bytes") return 'binary';
+  if (field.type === "Boolean") return 'boolean';
+  if (field.type === "int") return 'integer';
+  if (field.type === "BigInt") return 'bigInteger';
+  if (field.type === "Float") return 'float';
+  if (field.type === "Decimal") return 'decimal';
+
+  // TODO: handle foreign ids from field relations
+
+  return 'string';
 }
