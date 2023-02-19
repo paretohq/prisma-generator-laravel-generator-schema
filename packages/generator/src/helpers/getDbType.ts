2dc2b4802a932d1d2f28f511f4d49d9a0da53f6c
Mosaab_Emam <mosaab.emam123@gmail.com>
2023-02-19 09:32:50 +0200

    fix(getDbType): infer id type from name or spec

--- packages/generator/src/helpers/getDbType.ts
+++ packages/generator/src/helpers/getDbType.ts
@@ -1,6 +1,10 @@
 import { DMMF } from "@prisma/generator-helper";
 
 export function getDbType(field: DMMF.Field): String {
+  // infer db type from field name
+  if (field.name === 'id' || field.isId) return 'id';
+
+  // infer db type from field type
   if (field.type === "String") return 'string';
   if (field.type === "DateTime") return 'datetime';
   if (field.type === "JSON") return 'json';
