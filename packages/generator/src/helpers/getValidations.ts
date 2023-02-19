0cfcd7f34acea186da26e7f34f8a059661516e5f
Mosaab_Emam <mosaab.emam123@gmail.com>
2023-02-19 15:08:29 +0200

    feat(getValidations): infer some sane defaults

--- packages/generator/src/helpers/getValidations.ts
+++ packages/generator/src/helpers/getValidations.ts
@@ -1,5 +1,38 @@
 import { DMMF } from "@prisma/generator-helper";
 
 export function getValidations(field: DMMF.Field): String {
-  return "";
+  const rules = [];
+
+  if (field.isRequired)
+    rules.push("required")
+
+  // TODO: unique rules needs access to db name of model
+
+  if (field.type === "String")
+    rules.push("string")
+
+  if (field.type === "Float" || field.type === "Decimal")
+    rules.push("decimal")
+
+  if (field.type === "Boolean")
+    rules.push("boolean")
+
+  if (field.name === "id")
+    rules.push("integer|min:1")
+
+  if (field.name === "email")
+    rules.push("email")
+
+  if (field.name === "password")
+    rules.push("min:8")
+
+  // TODO: option in custom comment to set min,max,length rules
+
+  // TODO: option to add url rule
+
+  // TODO: option in relationship fields to add exists rule
+
+  // TODO: option to override inferred rules entirely
+
+  return rules.join("|");
 }
